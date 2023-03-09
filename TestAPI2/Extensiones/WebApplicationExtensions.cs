d64a3a786e601a6469c04263cde55f8ab3916cef
i.izurza <i.izurza@DESKTOP-5D225VS>
2023-03-09 13:28:15 +0100

    Including Serilog ~

--- TestAPI2/Extensiones/WebApplicationExtensions.cs
+++ TestAPI2/Extensiones/WebApplicationExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace TestAPI2.Extensiones;
+﻿using Serilog;
+using Serilog.Events;
+
+namespace TestAPI2.Extensiones;
 
 public static class WebApplicationExtensions
 {
@@ -9,6 +12,26 @@ public static class WebApplicationExtensions
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+        return app;
+    }
+
+    public static WebApplication UseConfiguredSerilogRequestLogging(this WebApplication app)
+    {
+        app.UseSerilogRequestLogging(options =>
+        {
+            // Customize the message template
+            options.MessageTemplate = "Handled {RequestPath}";
+
+            // Emit debug-level events instead of the defaults
+            options.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Debug;
+
+            // Attach additional properties to the request completion event
+            options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+            {
+                diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+                diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+            };
+        });
         return app;
     }
 }
--- TestAPI2/Program.cs
+++ TestAPI2/Program.cs
@@ -3,57 +3,85 @@ using TestAPI2.Services;
 using TestAPI2.Services.Interfaces;
 using Carter;
 using TestAPI2.Extensiones;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .WriteTo.Console()
+    //.WriteTo.Debug()
+    .CreateBootstrapLogger();
+    //.CreateLogger();
 
-string _connectionString = builder.Configuration.GetConnectionString("LocalDB");
+try
+{
+    Log.Information("Starting web application");
+    var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.ScanServices();
-//builder.Services.Scan(s => s
-    //.FromAssemblyOf<IApiService>()
-    //.AddClasses(c => c.AssignableTo<IApiService>())
-    //.AsImplementedInterfaces()
-    //.WithScopedLifetime());
-
-builder.Services.ConfigureJsonOption();//Replaces function below
-// Customize the JSON serialization options used by minimal with following line
-//builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(o => 
-//    {
-//        o.SerializerOptions.IncludeFields = true;
-//        //o.SerializerOptions.MaxDepth = 0;
-//        o.SerializerOptions.PropertyNameCaseInsensitive = true;
-//        o.SerializerOptions.WriteIndented = true;
-//        o.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-//    });
-
-//builder.Services.AddControllers(); // Not needed on Minimal APIs
-
-//builder.Services.AddScoped<IProductService, ProductService>();
-//builder.Services.AddScoped<IVentaService, VentaService>();
-//builder.Services.AddScoped<IClientService, ClientService>();
+    //builder.Host.UseSerilog();
+  
+    string _connectionString = builder.Configuration.GetConnectionString("LocalDB");
 
 
-builder.Services.AddPersistence(_connectionString);// Replaces the line below
-//builder.Services.AddDbContext<VentaContext>(opt => opt.UseSqlServer(_connectionString ?? throw new Exception("Missing Connection String"))/*, ServiceLifetime.Transient*/);
+    builder.Services.ScanServices();
+    //builder.Services.Scan(s => s
+        //.FromAssemblyOf<IApiService>()
+        //.AddClasses(c => c.AssignableTo<IApiService>())
+        //.AsImplementedInterfaces()
+        //.WithScopedLifetime());
 
-builder.Services.AddSwagger();//Replaces the 2 below
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+    builder.Services.ConfigureJsonOption();//Replaces function below
+    // Customize the JSON serialization options used by minimal with following line
+    //builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(o => 
+    //    {
+    //        o.SerializerOptions.IncludeFields = true;
+    //        //o.SerializerOptions.MaxDepth = 0;
+    //        o.SerializerOptions.PropertyNameCaseInsensitive = true;
+    //        o.SerializerOptions.WriteIndented = true;
+    //        o.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    //    });
 
-builder.Services.AddCarter();
+    builder.Services.AddPersistence(_connectionString);// Replaces the line below
+    //builder.Services.AddDbContext<VentaContext>(opt => opt.UseSqlServer(_connectionString ?? throw new Exception("Missing Connection String"))/*, ServiceLifetime.Transient*/);
 
-var app = builder.Build();
+    builder.Services.AddSwagger();//Replaces the 2 below
+    //builder.Services.AddEndpointsApiExplorer();
+    //builder.Services.AddSwaggerGen();
+
+    builder.Services.AddCarter();
 
 
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+      .ReadFrom.Configuration(context.Configuration)
+      .ReadFrom.Services(services)
+      .Enrich.FromLogContext()
+      .WriteTo.File(new CompactJsonFormatter(), "./logs/myapp.json")
+      .WriteTo.Console());
 
-app.MapSwagger();
+    var app = builder.Build();
 
-app.UseHttpsRedirection();
+    //app.UseConfiguredSerilogRequestLogging();
+    app.UseSerilogRequestLogging();//Set before noisy handlers to exclude them from logging
+    
 
-//app.UseAuthorization();
+    app.MapSwagger();
 
-//app.MapControllers();// Not needed on Minimal APIs
-app.MapCarter();
+    app.UseHttpsRedirection();
 
-app.Run();
+    //app.UseAuthorization();
+
+    app.MapCarter();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
